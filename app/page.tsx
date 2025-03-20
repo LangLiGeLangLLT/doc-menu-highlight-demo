14a95efc1359d27121a12d39a17d758cca0b675a
llt <418797644@qq.com>
2025-03-20 22:48:41 +0800

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,92 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import React from 'react'
+import { items } from './items'
+import { throttle } from 'lodash-es'
+
+export default function Page() {
+  const menuItems = React.useRef<HTMLAnchorElement[]>([])
+  const sections = React.useRef<HTMLElement[]>([])
+
+  function handleScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+    sections.current.forEach((section, index) => {
+      const prevSection = sections.current[index - 1]
+      const sectionTop = section.offsetTop
+      const sectionHeight = section.offsetHeight
+
+      // 判断标题是否在视口中
+      if (sectionTop <= scrollTop && scrollTop < sectionTop + sectionHeight) {
+        // 找到对应的菜单项并高亮
+        const menuItem = document.querySelector(`[data-target="${section.id}"]`)
+        if (menuItem) {
+          menuItems.current.forEach((item) => item.classList.remove('active'))
+          menuItem.classList.add('active')
+        }
+        return
+      }
+      if (
+        prevSection &&
+        prevSection.offsetTop + prevSection.offsetHeight <= scrollTop &&
+        scrollTop < sectionTop
+      ) {
+        // 找到对应的菜单项并高亮
+        const menuItem = document.querySelector(`[data-target="${section.id}"]`)
+        if (menuItem) {
+          menuItems.current.forEach((item) => item.classList.remove('active'))
+          menuItem.classList.add('active')
+        }
+      }
+    })
+  }
+
+  React.useEffect(() => {
+    menuItems.current = Array.from(document.querySelectorAll('[data-target]'))
+    sections.current = Array.from(document.querySelectorAll('section'))
+
+    handleScroll()
+
+    const onScroll = throttle(handleScroll, 100)
+
+    window.addEventListener('scroll', onScroll)
+    window.addEventListener('resize', onScroll)
+
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+      window.removeEventListener('resize', onScroll)
+    }
+  }, [])
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
+    <div className="flex space-x-4">
+      <ul className="sticky left-0 top-0 h-fit menu space-y-4">
+        {items.map((item) => (
+          <li key={item.id} className="w-[100px]">
+            <a
+              href={`#${item.id}`}
+              data-target={item.id}
+              onClick={(e) => {
+                e.preventDefault()
+                const target = document.querySelector(
+                  `#${(e.target as HTMLAnchorElement).dataset.target}`
+                )
+                target?.scrollIntoView({ behavior: 'smooth' })
+              }}
+            >
+              {item.name}
+            </a>
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        ))}
+      </ul>
+      <div className="space-y-[100px]">
+        {items.map((item) => (
+          <section key={item.id} id={item.id} className="section">
+            <h2 className="text-2xl font-bold">{item.name}</h2>
+            {item.content}
+          </section>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
